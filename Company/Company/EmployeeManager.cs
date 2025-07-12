50e27117001c8bc865fb2c04305304c5426898cb
MikruykovaEkaterina <olakudrina687@gmail.com>
2025-07-12 11:16:39 +0400

    Добавила документацию
    Добавила документацию, регионы. сделала исключения более информативными

--- Company/Company/EmployeeManager.cs
+++ Company/Company/EmployeeManager.cs
@@ -7,56 +7,83 @@ using System.Xml.Linq;
 
 namespace Company
 {
+  /// <summary>
+  /// Класс, отвечающий за управление сотрудниками.
+  /// </summary>
+  /// <typeparam name="T">Тип сотрудника.</typeparam>
   internal class EmployeeManager<T> : IEmployeeManager<T> where T : Employee
   {
+    #region Поля и свойства
+
+    /// <summary>
+    /// Внутренняя коллекция для хранения сотрудников
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Использует обобщенный список для хранения объектов типа <typeparamref name="T"/> в памяти.
+    /// Доступ к коллекции осуществляется исключительно через публичные методы менеджера.
+    /// </para>
+    /// </remarks>
     private List<T> employees = new List<T>();
 
+    #endregion
+
+    #region <IEmployeeManager<T>>
+
+    /// <inheritdoc/>
+    /// <exception cref="EmployeeAdditionException">Вызывается, если сотрудник уже добавлен или передано значение null.</exception>
     public void Add(T employee)
     {
       if (employee == null)
         throw new EmployeeAdditionException("Нельзя добавить пустого сотрудника");
       T? results = employees.SingleOrDefault(emp => emp.ID == employee.ID);
       if (results != null)
-        throw new EmployeeAdditionException($"Сотрудник с таким id уже добавлен");
-      //все проверки пройдены, можно добавлять
+        throw new EmployeeAdditionException($"Сотрудник с id = {employee.ID} уже добавлен");
       employees.Add(employee);
     }
 
-    public T Get(string name)
-    {
-      T? result = employees.FirstOrDefault(emp => emp.Name == name); //имя не уникально
-      //я бы лучше возвращала список
-      if (result == null) 
-        throw new EmployeeNotFoundException("Сотрудник с таким именем не содержится в списке");
-      return result;
-    }
-
+    /// <inheritdoc/>
+    /// <exception cref="EmployeeNotFoundException">Вызывается, если сотрудник не найден в списке.</exception>
+    /// <exception cref="InvalidOperationException">Вызывается, если найдено несколько сотрудников с одним и тем же ID.</exception>
     public void Update(T employee)
     {
-      T employeeToUpdateInfo = GetById (employee.ID);
+      T employeeToUpdateInfo = GetById(employee.ID);
       int index = employees.IndexOf(employeeToUpdateInfo);
       employees[index] = employee;
     }
 
+    /// <inheritdoc/>
+    /// <exception cref="EmployeeNotFoundException">Вызывается, если сотрудник не найден в списке.</exception>
     public void Delete(T employee)
     {
-      Console.WriteLine(employee);
-      if (!employees.Contains(employee)) 
-        throw new EmployeeNotFoundException("Такой сотрудник не содержится в списке");
+      if (!employees.Contains(employee))
+        throw new EmployeeNotFoundException($"Cотрудник: Имя = {employee.Name}, ID = {employee.ID} не содержится в списке");
       employees.Remove(employee);
     }
 
+    /// <inheritdoc/>
+    /// <exception cref="EmployeeNotFoundException">Вызывается, если сотрудник не найден в списке.</exception>
+    /// <exception cref="InvalidOperationException">Вызывается, если найдено несколько сотрудников с одним и тем же ID.</exception>
     public T GetById(int id)
     {
-      T? result = employees.SingleOrDefault(emp => emp.ID == id); //id должно быть уникально
-      if (result == null) 
-        throw new EmployeeNotFoundException("Сотрудник с таким id не содержится в списке");
-      return result;
+      try
+      {
+        T? result = employees.SingleOrDefault(emp => emp.ID == id);
+        if (result == null)
+          throw new EmployeeNotFoundException($"Сотрудник с id = {id} не содержится в списке");
+        return result;
+      }
+      catch (InvalidOperationException ex)
+      {
+        throw new InvalidOperationException($"Найдено несколько сотрудников с ID = {id}");
+      }
     }
 
     public List<T> getEmployees()
     {
       return employees;
     }
+
+    #endregion
   }
 }
