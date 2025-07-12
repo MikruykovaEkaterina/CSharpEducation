c0bfb179fc55ee238af0d855550347ec378df740
MikruykovaEkaterina <olakudrina687@gmail.com>
2025-07-12 11:17:25 +0400

    Добавила документация
    Добавила документацию, регионы. Сделала исключения более информативными

--- Company/Company/FullTimeEmployee.cs
+++ Company/Company/FullTimeEmployee.cs
@@ -6,40 +6,73 @@ using System.Threading.Tasks;
 
 namespace Company
 {
-  internal class FullTimeEmployee: Employee
+  /// <summary>
+  /// Представляет сотрудника с полной занятостью и фиксированным окладом
+  /// </summary>
+  /// <remarks>
+  /// Наследуется от абстрактного класса <see cref="Employee"/>.
+  /// Зарплата рассчитывается как фиксированный оклад без дополнительных начислений.
+  /// </remarks>
+  internal class FullTimeEmployee : Employee
   {
+    #region Базовый класс
+
     private int id;
+    /// <inheritdoc/>
+    /// <exception cref="ArgumentException">
+    /// Выбрасывается при попытке установить не положительное значение.
+    /// </exception>
     public override int ID
     {
-      get { return id; }
+      get
+      {
+        return id;
+      }
       set
       {
         if (value <= 0)
-          throw new ArgumentException($"Введено недопустимое значение для (ID). ID должен быть положительным");
+          throw new ArgumentException($"Введено недопустимое значение для (ID = {value}). " +
+            $"ID должен быть положительным");
         id = value;
       }
     }
 
     private string name;
+    /// <inheritdoc/>
+    /// <exception cref="ArgumentException">
+    /// Выбрасывается при попытке установить пустую строку.
+    /// </exception>
     public override string Name
     {
-      get { return name; }
+      get
+      {
+        return name;
+      }
       set
       {
         if (string.IsNullOrWhiteSpace(value))
-          throw new ArgumentException($"Введено недопустимое значение для (Имя). Имя не может быть пустым");
+          throw new ArgumentException($"Введено недопустимое значение для (Имя = '{value}'). " +
+            $"Имя не может быть пустым");
         name = value;
       }
     }
 
     private decimal baseSalary;
+    /// <inheritdoc/>
+    /// <exception cref="ArgumentException">
+    /// Выбрасывается при попытке установить не положительное значение.
+    /// </exception>
     public override decimal BaseSalary
     {
-      get { return baseSalary; }
+      get
+      {
+        return baseSalary;
+      }
       set
       {
-        if (value<=0)
-          throw new ArgumentException($"Введено недопустимое значение для (Зарплата). Зарплата должна быть положительной");
+        if (value <= 0)
+          throw new ArgumentException($"Введено недопустимое значение для (Зарплата = {value}). " +
+            $"Зарплата должна быть положительной");
         baseSalary = value;
       }
     }
@@ -49,6 +82,24 @@ namespace Company
       return BaseSalary;
     }
 
+    public override string ToString()
+    {
+      return $"ID: {ID}; Имя: {Name}; Зарплата: {BaseSalary}";
+    }
+
+    #endregion
+
+    #region Конструкторы
+
+    /// <summary>
+    /// Инициализирует новый экземпляр класса <see cref="FullTimeEmployee"/>
+    /// </summary>
+    /// <param name="id">Уникальный идентификатор сотрудника</param>
+    /// <param name="name">Полное имя сотрудника</param>
+    /// <param name="baseSalary">Фиксированный оклад сотрудника</param>
+    /// <exception cref="ArgumentException">
+    /// Возникает если любой из параметров не соответствует бизнес-правилам.
+    /// </exception>
     public FullTimeEmployee(int id, string name, decimal baseSalary)
     {
       ID = id;
@@ -56,9 +107,6 @@ namespace Company
       BaseSalary = baseSalary;
     }
 
-    public override string ToString()
-    {
-      return $"ID: {ID}; Имя: {Name}; Зарплата: {BaseSalary}";
-    }
+    #endregion
   }
 }
