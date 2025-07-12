943aff58cc915bfff7d7f6b09f93cd62829d6abd
MikruykovaEkaterina <olakudrina687@gmail.com>
2025-07-12 11:17:41 +0400

    Добавила документацию

--- Company/Company/IEmployeeManager.cs
+++ Company/Company/IEmployeeManager.cs
@@ -6,11 +6,41 @@ using System.Threading.Tasks;
 
 namespace Company
 {
+  /// <summary>
+  /// Интерфейс, определяющий основные операции управления сотрудниками.
+  /// </summary>
+  /// <typeparam name="T">Тип сотрудника</typeparam>
   internal interface IEmployeeManager<T>
   {
+    /// <summary>
+    /// Добавляет нового сотрудника.
+    /// </summary>
+    /// <param name="employee">Сотрудник для добавления.</param>
     void Add(T employee);
-    T Get(string name);
+
+    /// <summary>
+    /// Возвращает сотрудника по указанному идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор сотрудника.</param>
+    /// <returns>Сотрудник с указанным идентификатором.</returns>
+    T GetById(int id);
+
+    /// <summary>
+    /// Обновляет информацию о сотруднике.
+    /// </summary>
+    /// <param name="employee">Сотрудник для обновления.</param>
     void Update(T employee);
+
+    /// <summary>
+    /// Удаляет сотрудника.
+    /// </summary>
+    /// <param name="employee">Сотрудник для удаления.</param>
     void Delete(T employee);
+
+    /// <summary>
+    /// Возвращает список всех сотрудников.
+    /// </summary>
+    /// <returns>Список всех сотрудников.</returns>
+    List<T> getEmployees();
   }
 }
